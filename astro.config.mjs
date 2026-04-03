a5e9862bf1aca81addaac2c3c56155984f04f8f2
Jana Unterreiner <jana.unterreiner@advastore.com>
2026-04-03 14:23:22 +0200

    korrektur output hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'hybrid',
+//   output: 'hybrid',
   site: 'https://unterreinerjana.github.io',
   base: '/janas_recipes',
 });
